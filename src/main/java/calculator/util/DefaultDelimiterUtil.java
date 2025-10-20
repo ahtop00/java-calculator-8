64a19ef2ee26a425c47008a7ccd02346d33c5573
KYS <ehcl1027@gmail.com>
2025-10-20 14:56:19 +0900

    feat: 기본 구분자 유틸리티 클래스 추가
    기본 구분자인 쉼표(,)와 콜론(:)을 상수로 관리하기 위한 DefaultDelimiterUtil 클래스를 추가합니다.
    
    - 매직넘버를 제거하여 코드의 명확성을 향상시킵니다.
    - private 생성자를 추가하여 유틸리티 클래스의 인스턴스 생성을 방지합니다.

---
+++ src/main/java/calculator/util/DefaultDelimiterUtil.java
@@ -0,0 +1,8 @@
+package calculator.util;
+
+public class DefaultDelimiterUtil {
+    public final static String COLON = ":";
+    public final static String COMMA = ",";
+
+    private DefaultDelimiterUtil() {}
+}
