9413cd684e3c3a62b8bc03c3f87c5e5fa576e7f5
KYS <ehcl1027@gmail.com>
2025-10-20 16:11:00 +0900

    feat: Application 구현 및 의존성 주입
    - main 메서드 내에서 애플리케이션 실행에 필요한 모든 객체를 생성합니다.
    - 생성자 주입(DI)을 통해 각 객체의 의존성을 설정 후에, Controller를 중심으로 전체 컴포넌트를 조립합니다.
    - calculatorController.run()을 호출하여 실제 애플리케이션 로직을 실행합니다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
+import calculator.domain.Calculator;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+
+        CalculatorController calculatorController = new CalculatorController(
+                calculator, inputView, outputView);
+
+        calculatorController.run();
     }
 }
