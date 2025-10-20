75c358c0e1136aa0bb05fd96be1e084e808d159b
KYS <ehcl1027@gmail.com>
2025-10-20 15:48:58 +0900

    feat: 덧셈 기능 구현 (빈 문자열, 숫자 하나)
    - 입력값이 null이거나 빈 문자열일 경우 0을 반환하는 기능을 구현합니다.
    - 입력값이 숫자 하나일 경우 해당 숫자를 정수로 변환하여 반환합니다.

---
+++ src/main/java/calculator/domain/Calculator.java
@@ -0,0 +1,10 @@
+﻿package calculator.domain;
+
+public class Calculator {
+    public int add(String expression) {
+        if (expression == null || expression.isBlank()) {
+            return 0;
+        }
+        return Integer.parseInt(expression);
+    }
+}
