2785ba043d198350c2b8b951f8ad3f93b84a9560
KYS <ehcl1027@gmail.com>
2025-10-20 14:58:03 +0900

    feat: 커스텀 구분자 파싱용 유틸리티 클래스 추가
    커스텀 구분자 문자열을 파싱하는 데 사용되는 접두사('//')와 접미사('\n')를 상수로 관리하기 위해 `CustomDelimiterUtil` 클래스를 추가합니다.
    
    - 파싱 로직에 사용되는 매직 넘버를 제거하여 가독성을 높입니다.
    - private 생성자를 추가하여 유틸리티 클래스의 인스턴스 생성을 방지합니다.

---
+++ src/main/java/calculator/util/CustomDelimiterUtil.java
@@ -0,0 +1,8 @@
+package calculator.util;
+
+public class CustomDelimiterUtil {
+    public final static String PREFIX = "//";
+    public final static String SUFFIX = "\\n";
+
+    private CustomDelimiterUtil() {}
+}
