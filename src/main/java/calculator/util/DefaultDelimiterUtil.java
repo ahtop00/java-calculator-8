d6dfca626513dbf400d962fc6d9e46adc03698b8
KYS <ehcl1027@gmail.com>
2025-10-20 15:56:15 +0900

    feat: 기본 구분자 정규식 구현

--- src/main/java/calculator/util/DefaultDelimiterUtil.java
+++ src/main/java/calculator/util/DefaultDelimiterUtil.java
@@ -3,6 +3,7 @@ package calculator.util;
 public class DefaultDelimiterUtil {
     public final static String COLON = ":";
     public final static String COMMA = ",";
+    public final static String DEFAULT_DELIMITER_REGEX = COLON + "|" + COMMA;
 
     private DefaultDelimiterUtil() {}
 }
