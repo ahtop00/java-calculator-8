ee1fac4522769397434e4f66e644760124794a59
KYS <ehcl1027@gmail.com>
2025-10-20 15:57:01 +0900

    feat: 기본 구분자(쉼표, 콜론)를 이용한 덧셈 기능 구현
    - 쉼표(,) 또는 콜론(:)을 구분자로 사용하여 문자열을 분리하고 각 숫자의 합을 반환하는 기능을 추가합니다.
    - Stream API를 사용하여 숫자 변환 및 합산 로직을 구현합니다.

--- src/main/java/calculator/domain/Calculator.java
+++ src/main/java/calculator/domain/Calculator.java
@@ -1,10 +1,24 @@
-﻿package calculator.domain;
+package calculator.domain;
+
+import calculator.util.DefaultDelimiterUtil;
+import java.util.Arrays;
 
 public class Calculator {
     public int add(String expression) {
         if (expression == null || expression.isBlank()) {
             return 0;
         }
-        return Integer.parseInt(expression);
+        String[] numberStrings = expression.split(DefaultDelimiterUtil.DEFAULT_DELIMITER_REGEX);
+        return sum(numberStrings);
+    }
+
+    private int sum(String[] numberStrings) {
+        return Arrays.stream(numberStrings)
+                .mapToInt(this::toInt)
+                .sum();
+    }
+
+    private int toInt(String numberString) {
+        return Integer.parseInt(numberString);
     }
 }
